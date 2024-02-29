fee6f0d5691dbb429b5ef9b774eddb90b49262d0
Cheikh Sidi El Moctar <cheikh@blameless.com>
2024-02-29 15:31:25 -0500

    ENG-1772: run tsc

--- src/service/router.test.ts
+++ src/service/router.test.ts
@@ -4,12 +4,31 @@ import request from 'supertest';
 
 import { createRouter } from './router';
 
+jest.mock('@backstage/config', () => {
+  return {
+    loadConfig: jest.fn().mockResolvedValue({}),
+  };
+});
+
+jest.mock('@backstage/backend-common', () => {
+  return {
+    ...jest.requireActual('@backstage/backend-common'),
+    PluginEndpointDiscovery: {
+      fromConfig: jest.fn().mockResolvedValue({
+        getBaseUrl: () => 'http://localhost:7000',
+      }),
+    },
+  };
+});
+
 describe('createRouter', () => {
   let app: express.Express;
 
   beforeAll(async () => {
     const router = await createRouter({
       logger: getVoidLogger(),
+      config: {} as any,
+      discovery: {} as any,
     });
     app = express().use(router);
   });
