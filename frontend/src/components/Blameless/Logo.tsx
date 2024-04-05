af8a4a9555402fdb5b5e453dfaf58de16478f15a
Cheikh Sidi El Moctar <cheikh@blameless.com>
2024-04-05 13:29:36 -0400

    ENG-1928: Fixed tsc issues

--- frontend/src/components/Blameless/Logo.tsx
+++ frontend/src/components/Blameless/Logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Blameless from '../../../assets/blameless.png';
+import Blameless from '../../assets/blameless.png';
 
 export const BlamelessLogo = () => {
-  return <img src={Blameless} height={22} width={22}/>;
+  return <img src={Blameless} height={22} width={22} alt="blameless-logo"/>;
 };
--- frontend/src/components/BlamelessFetch/IncidentFetchComponent.tsx
+++ frontend/src/components/BlamelessFetch/IncidentFetchComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Table, TableColumn, Progress, ResponseErrorPanel } from '@backstage/core-components';
 import {SearchBar} from '@backstage/plugin-search-react';
